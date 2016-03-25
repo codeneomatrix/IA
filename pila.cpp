2a458c904601ccb87479ca41dfeb1fdf4bd61bf7
neomatrix <josuecevedo@gmail.com>
2016-03-24 19:05:39 -0600

    update profundidad

--- pila.cpp
+++ pila.cpp
@@ -12,37 +12,23 @@ int ct=0;
 
 void Push(Pila *pila, int v) {
    pNodo nuevo;
- 
-   /* Crear un nodo nuevo */
    nuevo = (pNodo)malloc(sizeof(tipoNodo));
    nuevo->valor = v;
-   
-   /* Añadimos la pila a continuación del nuevo nodo */
    nuevo->siguiente = *pila;
-   /* Ahora, el comienzo de nuestra pila es en nuevo nodo */
    *pila = nuevo;
    ct=ct+1;
 }
 
-int cantidad(){
-   return ct;
-}
+int cantidad(){ return ct;}
 
 int Pop(Pila *pila) {
-   pNodo nodo; /* variable auxiliar para manipular nodo */
-   int v;      /* variable auxiliar para retorno */
-   
-   /* Nodo apunta al primer elemento de la pila */
+   pNodo nodo; 
+   int v; 
    nodo = *pila;
-   if(!nodo) return -1; /* Si no hay nodos en la pila retornamos 0 */
-   /* Asignamos a pila toda la pila menos el primer elemento */
+   if(!nodo) return -1; 
    *pila = nodo->siguiente;
-   /* Guardamos el valor de retorno */
    v = nodo->valor; 
-   /* Borrar el nodo */
    free(nodo);
    ct=ct-1;
    return v;
 }
-
-
