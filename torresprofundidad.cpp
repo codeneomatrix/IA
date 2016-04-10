8868d33eff80140309c87c2659ba2356a0d58c5d
neomatrix <josuecevedo@gmail.com>
2016-04-10 00:05:09 -0500

    hanoi profundidad METAPROGRAMADO

--- torresprofundidad.cpp
+++ torresprofundidad.cpp
@@ -6,16 +6,16 @@
   Description:
 */
 
-#include <stdio.h>
+#define ndiscos 3
 
+#include <stdio.h>
 #include "torrespila.cpp"
-#include "pila.cpp"
 
 int final = 2;
 int pt = 21;
 
 int agujadestino = 2;
-
+int discos=ndiscos;
 Pilat ph = NULL;
 
 void sabermovimiento(int vd, int co, int cd) {
@@ -36,10 +36,10 @@ int proximo(int v, int i) {
   return v;
 }
 
-void imprimirmatrix(int matrix[3][9]) {
-  printf("\t\t\t\t<%d>\t<%d>\t<%d>\n", matrix[0][8], matrix[1][8],
-         matrix[2][8]);
-  for (int j = 7; j >= 0; j--) {
+void imprimirmatrix(int matrix[3][ndiscos]) {
+  printf("\t\t\t\t<%d>\t<%d>\t<%d>\n", matrix[0][discos], matrix[1][discos],
+         matrix[2][discos]);
+  for (int j = discos-1; j >= 0; j--) {
     printf("\t\t\t\t");
     for (int i = 0; i < 3; i++) {
       if (matrix[i][j] != 10) {
@@ -53,6 +53,7 @@ void imprimirmatrix(int matrix[3][9]) {
   printf("\t\t--------------------------------------------------\n");
 }
 
+
 bool busqueda(int profundidad) {
   if (profundidad < pt) {
     // printf(
@@ -64,39 +65,39 @@ bool busqueda(int profundidad) {
         int pd = proximo(p, j);
 
         if (nivelpila(&ph, p) > -1 && p != pd) {
-          int matrixtem[3][9];
+          int matrixtem[3][discos+1];
 
           int *to = datostorre(&ph, 0);
-          for (int i = 0; i < 8; i++) {
+          for (int i = 0; i < discos; i++) {
             matrixtem[0][i] = to[i];
           }
 
-          matrixtem[0][8] = nivelpila(&ph, 0);
+          matrixtem[0][discos] = nivelpila(&ph, 0);
 
           to = datostorre(&ph, 1);
-          for (int i = 0; i < 8; i++) {
+          for (int i = 0; i < discos; i++) {
             matrixtem[1][i] = to[i];
           }
-          matrixtem[1][8] = nivelpila(&ph, 1);
+          matrixtem[1][discos] = nivelpila(&ph, 1);
 
           to = datostorre(&ph, 2);
-          for (int i = 0; i < 8; i++) {
+          for (int i = 0; i < discos; i++) {
             matrixtem[2][i] = to[i];
           }
-          matrixtem[2][8] = nivelpila(&ph, 2);
+          matrixtem[2][discos] = nivelpila(&ph, 2);
 
           int vdo;
 
-          if (matrixtem[p][8] >= 0) {
-            vdo = matrixtem[p][matrixtem[p][8]];
+          if (matrixtem[p][discos] >= 0) {
+            vdo = matrixtem[p][matrixtem[p][discos]];
           } else {
             vdo = 10;
           }
 
           int vdd;
 
-          if (matrixtem[pd][8] >= 0) {
-            vdd = matrixtem[pd][matrixtem[pd][8]];
+          if (matrixtem[pd][discos] >= 0) {
+            vdd = matrixtem[pd][matrixtem[pd][discos]];
           } else {
             vdd = 10;
           }
@@ -104,32 +105,32 @@ bool busqueda(int profundidad) {
           if (vdo < vdd && vdo != 10) {
             // printf("\t\t\t\t\t {%d,%d}\n", p, pd);
             // printf("--[>%d ",profundidad );
-            // sabermovimiento(matrixtem[p][matrixtem[p][8]], p, pd);
+            // sabermovimiento(matrixtem[p][matrixtem[p][discos]], p, pd);
 
-            matrixtem[pd][8] += 1;
+            matrixtem[pd][discos] += 1;
 
-            matrixtem[pd][matrixtem[pd][8]] = matrixtem[p][matrixtem[p][8]];
-            matrixtem[p][matrixtem[p][8]] = 10;
-            matrixtem[p][8] -= 1;
+            matrixtem[pd][matrixtem[pd][discos]] = matrixtem[p][matrixtem[p][discos]];
+            matrixtem[p][matrixtem[p][discos]] = 10;
+            matrixtem[p][discos] -= 1;
 
             // imprimirmatrix(matrixtem);
           }
 
-          int a1[8];
-          for (int i = 0; i < 8; i++) {
+          int a1[discos];
+          for (int i = 0; i < discos; i++) {
             a1[i] = matrixtem[0][i];
           }
-          int a2[8];
-          for (int i = 0; i < 8; i++) {
+          int a2[discos];
+          for (int i = 0; i < discos; i++) {
             a2[i] = matrixtem[1][i];
           }
-          int a3[8];
-          for (int i = 0; i < 8; i++) {
+          int a3[discos];
+          for (int i = 0; i < discos; i++) {
             a3[i] = matrixtem[2][i];
           }
 
-          Push(&ph, a1, a2, a3, matrixtem[0][8], matrixtem[1][8],
-               matrixtem[2][8]);
+          Push(&ph, a1, a2, a3, matrixtem[0][discos], matrixtem[1][discos],
+               matrixtem[2][discos]);
 
           if (nivelpila(&ph, agujadestino) == final) {
             printf("\nENCONTRADO!!\n");
@@ -150,38 +151,46 @@ bool busqueda(int profundidad) {
 
 int main() {
 
-  int aguja1[] = {8, 7, 6, 5, 4, 3, 2, 1};
-  int agujar[] = {10, 10, 10, 10, 10, 10, 10, 10};
+  int aguja1[discos];
+  int agujar[discos];
+
+	for (int i = 0; i < discos; i++) {
+		aguja1[i]=discos-i;
+	}
+	for (int i = 0; i < discos; i++) {
+		agujar[i]=10;
+	}
 
   printf("\n\n\n\n\t\t\t\t CALCULANDO...\n ");
 
-  Push(&ph, aguja1, agujar, agujar, 7, -1, -1);
+  Push(&ph, aguja1, agujar, agujar, discos-1, -1, -1);
 
   busqueda(0);
 
   //------------------------------------------------
-  int matrixtem[3][9];
+
+  int matrixtem[3][ndiscos];
 
   while (((nivelpila(&ph, 0) != -1) || (nivelpila(&ph, 1) != -1)) ||
          nivelpila(&ph, 2) != -1) {
     int *to = datostorre(&ph, 0);
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < discos; i++) {
       matrixtem[0][i] = to[i];
     }
 
-    matrixtem[0][8] = nivelpila(&ph, 0);
+    matrixtem[0][discos] = nivelpila(&ph, 0);
 
     to = datostorre(&ph, 1);
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < discos; i++) {
       matrixtem[1][i] = to[i];
     }
-    matrixtem[1][8] = nivelpila(&ph, 1);
+    matrixtem[1][discos] = nivelpila(&ph, 1);
 
     to = datostorre(&ph, 2);
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < discos; i++) {
       matrixtem[2][i] = to[i];
     }
-    matrixtem[2][8] = nivelpila(&ph, 2);
+    matrixtem[2][discos] = nivelpila(&ph, 2);
 
     imprimirmatrix(matrixtem);
     Pop(&ph);
