5748743e68f18846f72d049c9ba08fa682f8bc81
codeneomatrix <josuecevedo@gmail.com>
2017-05-10 20:30:11 -0500

    hanoi por anchura y n discos

---
+++ hanoianchura.cpp
@@ -0,0 +1,219 @@
+/*
+  program:3
+  Name: torresanchura.cpp
+  Author: josue acevedo maldonado
+  Date: 07/04/16
+  Description:
+*/
+#define ndiscos 3  // 2
+#define cantidad_de_estados 23  //8
+
+
+//valor que significa fin de datos 
+#define marca_de_fin  ndiscos+10
+// valor que representa disco nulo
+#define disco_nulo  ndiscos+2
+//valor que significa vacio 
+#define vacio ndiscos+20
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "listaestados.cpp"
+
+int discos = ndiscos;
+int todos_los_discos = ndiscos - 1;
+int discos_en_objetivo;
+int torre_objetivo = 2;
+
+int siguiente_torre(int torre, int desplazamiento) {
+  torre += desplazamiento;
+  if (torre >= 3) {
+    torre -= 3;
+  }
+  if (torre >= 3) {
+    torre -= 3;
+  }
+  if (torre >= 3) {
+    torre -= 3;
+  }
+  return torre;
+}
+
+void imprimir(int s[]) {
+  int i =0;
+  int separador_torres =0;
+  int separador_estados=0;
+
+  while (s[i] < marca_de_fin) {
+
+    if (s[i] == disco_nulo){
+      printf("  [ ]");
+    }else{
+      printf("  [%d]", s[i]);  
+    }
+
+    if(separador_torres==discos) {
+      printf("\n");
+      separador_estados++;
+    }
+     if (separador_estados==3) printf("\n-----------------\n");
+
+     separador_torres++;
+     
+     if (separador_torres > discos) separador_torres=0;
+     if (separador_estados >= 3) separador_estados=0;
+     i++;
+    }
+}
+
+void imprimirdatos(int s[]) {
+  int i =0;
+  while (s[i] < marca_de_fin) {
+    printf("%d > ", s[i]); 
+    i++;
+  } 
+    
+}
+
+int main() {
+
+  printf("\t\t\t\t CALCULANDO... \n\n\t\t\t\t\n");
+
+  estado *n = NULL;
+  int y[1] = {marca_de_fin};
+
+  int torre_con_discos[discos];
+  int torrevacia[discos];
+
+  for (int i = 0; i < discos; i++) {
+    torre_con_discos[i] = discos - i;
+  }
+  for (int i = 0; i < discos; i++) {
+    torrevacia[i] = disco_nulo;
+  }
+
+  //printf("\n\n\n\n\t\t\t\t CALCULANDO...\n ");
+
+  /*al agregar por primer vez el estado es : 
+  datos en torre a => [ 3    2    1  ] 
+  datos en torre b => [nulo nulo nulo]
+  datos en torre c => [nulo nulo nulo]
+  discos en la torre a es todos_los_discos
+  discos en la torre b es 0
+  discos en la torre c es 0
+  el estado anterior es la marca de fin por que no existen estados anteriores
+  */
+  agregar(&n, torre_con_discos, torrevacia, torrevacia, discos-1, -1, -1, y);
+
+  discos_en_objetivo = obtener(&n,torre_objetivo);
+
+  while (discos_en_objetivo != todos_los_discos) {
+    int *previos = obteneranteriores(&n);
+    //printf("datos previos\n");
+    for (int torre = 0; torre < 3; torre++) {
+      for (int desplazamiento = 1; desplazamiento < 2; desplazamiento++) {
+
+          int numero_de_otra_torre = siguiente_torre(torre, desplazamiento);
+          //printf("combinacion\n");
+          // si la torre actual tiene discos (el -1 indica 0 discos) y no es la misma que la otra torre
+          if (obtener(&n, torre) > -1 && torre != numero_de_otra_torre) {
+
+                int matrixtem[3][discos + 1];
+
+                // se copia a la matriz temporal los discos de la torre a
+                int *to = datostorre(&n, 0);
+                for (int i = 0; i < discos; i++) {
+                  matrixtem[0][i] = to[i];
+                }
+                // se copia a la matriz temporal el numero de discos de la torre a
+                matrixtem[0][discos] = obtener(&n, 0);
+
+                // se copia a la matriz temporal los discos de la torre b
+                to = datostorre(&n, 1);
+                for (int i = 0; i < discos; i++) {
+                  matrixtem[1][i] = to[i];
+                }
+                // se copia a la matriz temporal el numero de discos de la torre b
+                matrixtem[1][discos] = obtener(&n, 1);
+
+                // se copia a la matriz temporal los discos de la torre c
+                to = datostorre(&n, 2);
+                for (int i = 0; i < discos; i++) {
+                  matrixtem[2][i] = to[i];
+                }
+                // se copia a la matriz temporal el numero de discos de la torre c
+                matrixtem[2][discos] = obtener(&n, 2);
+
+                //printf("matrix previa cargada\n");
+                int valor_disco_torre_inicial;
+
+                // si la torre actual tiene al menos 1 disco (el 0 indica 1 disco)
+                if (matrixtem[torre][discos] >= 0) {
+                  valor_disco_torre_inicial = matrixtem[torre][matrixtem[torre][discos]]; //por eso uso -1 para ningun disco , porque el numero de discos es al mismo tiempo el indice del disco en el arreglo
+                } else {
+                  valor_disco_torre_inicial = disco_nulo;
+                }
+
+                int valor_disco_torre_final;
+
+                if (matrixtem[numero_de_otra_torre][discos] >= 0) {
+                  valor_disco_torre_final = matrixtem[numero_de_otra_torre][matrixtem[numero_de_otra_torre][discos]];
+                } else {
+                  valor_disco_torre_final = disco_nulo;
+                }
+                //printf("valores de los discos determinados\n");
+                if (valor_disco_torre_inicial < valor_disco_torre_final && valor_disco_torre_inicial != disco_nulo) {
+                      // //printf("\t\t\t\t\t {%d,%d}\n", torre, numero_de_otra_torre);
+                      // //printf("--[>%d ",profundidad );
+                      // sabermovimiento(matrixtem[torre][matrixtem[torre][discos]], torre, numero_de_otra_torre);
+
+                      // el numero de discos en la otra torre aumenta en 1
+                      matrixtem[numero_de_otra_torre][discos] += 1;
+
+                      // se copia el disco de la torre inicial a la final
+                      matrixtem[numero_de_otra_torre][matrixtem[numero_de_otra_torre][discos]] =
+                          matrixtem[torre][matrixtem[torre][discos]];
+
+                      // el valor del ultimo disco en la torre actual se vuelve nulo
+                      matrixtem[torre][matrixtem[torre][discos]] = disco_nulo;
+                      // el numero de discos en la torre actual disminuye en 1
+                      matrixtem[torre][discos] -= 1;
+
+                      // imprimirmatrix(matrixtem);
+                      //printf("transformacion realizada\n");
+
+
+                    int nueva_torre_a[discos];
+                    for (int i = 0; i < discos; i++) {
+                      nueva_torre_a[i] = matrixtem[0][i];
+                    }
+
+                    int nueva_torre_b[discos];
+                    for (int i = 0; i < discos; i++) {
+                      nueva_torre_b[i] = matrixtem[1][i];
+                    }
+
+                    int nueva_torre_c[discos];
+                    for (int i = 0; i < discos; i++) {
+                      nueva_torre_c[i] = matrixtem[2][i];
+                    }
+                    //printf("agregando\n");
+                    agregar(&n, nueva_torre_a, nueva_torre_b, nueva_torre_c, matrixtem[0][discos], matrixtem[1][discos],
+                            matrixtem[2][discos], previos);
+                }
+          }
+      }
+    }
+
+    //-----------------------------------------------------------------------------
+    eliminar(&n);
+    discos_en_objetivo = obtener(&n,torre_objetivo);
+  }
+
+  int *previos = obteneranteriores(&n);
+  //imprimir(previos);
+  imprimir(previos);
+  imprimirdatos(previos);
+
+  return 0;
+}
---
+++ listaestados.cpp
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//valor que significa vacio 40
+//int vacio = 40;
+
+
+ //se almacena un numero finito de pasos o estados , siendo que cada estado almacena la posision de los discos por torre de cada torre ademas de el valor de la cantidad de discos por torre
+#define numero_total_de_datos  (((ndiscos+1)*3)*cantidad_de_estados)
+
+/*
+la estructura alberga los datos de un estado especicfico , como lo son la posision de los discos en cada torre,
+la cantidad de discos por torre, la posision de los discos y el numero de discos por torre de los estados
+anteriores y el actual en la variable todos ademas del puntero al estado siguiente 
+*/
+struct estado {
+  int torrea[ndiscos];
+  int torreb[ndiscos];
+  int torrec[ndiscos];
+  int datosena;
+  int datosenb;
+  int datosenc;
+  int todos[(numero_total_de_datos)];
+  struct estado *sig;
+};
+
+// variable que almacen el numero de estados en la lista
+int max = 0;
+
+// las funcion obtener sirve para poder recuperar el numero de discos 
+// presentes en las torres a, b y c, con los valores 0,1 y 2 respectivamente
+int obtener(struct estado **c1, int torre) {
+  if (torre == 0)
+    return (*c1)->sig->datosena;
+  if (torre == 1)
+    return (*c1)->sig->datosenb;
+  if (torre == 2)
+    return (*c1)->sig->datosenc;
+  return (*c1)->sig->datosenc;
+}
+
+
+// la funcion datostorre regresa el arreglo que representa la posision de los discos en 
+// cada torre c,b y c ; al pasarle el numero 0, 1 y 2 repectivamente
+int * datostorre(struct estado **c1, int t) {
+  if (t == 0)
+    return (*c1)->sig->torrea;
+  if (t == 1)
+    return (*c1)->sig->torreb;
+  if (t == 2)
+    return (*c1)->sig->torrec;
+  return (*c1)->sig->torrec;
+}
+
+int *obteneranteriores(struct estado **c1) { return (*c1)->sig->todos; }
+
+void agregar(struct estado **cola, int discos_en_a[], int discos_en_b[], int discos_en_c[], int cantidad_discos_a, int cantidad_discos_b,int cantidad_discos_c, int estados_anteriores[]){
+
+  //se crea un nuevo nodo de estado
+  struct estado *nuevo;
+
+  // se asigna memoria dinamica al nodo
+  nuevo = (struct estado *)malloc(sizeof(struct estado));
+
+  // se cargan la cantidad de discos en cada torre del nodo
+  nuevo->datosena = cantidad_discos_a;
+  nuevo->datosenb = cantidad_discos_b;
+  nuevo->datosenc = cantidad_discos_c;
+
+  // carga la posision de los discos en cada torre
+  for (int i = 0; i < ndiscos; i++) {
+    nuevo->torrea[i] = discos_en_a[i];
+  }
+  for (int i = 0; i < ndiscos; i++) {
+    nuevo->torreb[i] = discos_en_b[i];
+  }
+  for (int i = 0; i < ndiscos; i++) {
+    nuevo->torrec[i] = discos_en_c[i];
+  }
+
+  // en el arreglo que almacena los estados anteriores y el actual se le da "formato" se llena con un valor que indica nulo o vacio
+  for (int i = 0; i < numero_total_de_datos; i++) {
+    nuevo->todos[i] = vacio;
+  }
+
+  //in es una variable que nos permite saber en que posision del arreglo  
+  int in = 0;
+  // el arreglo que almacena los estados anteriores y el actual se carga con los datos de los nodos anteriores
+  for (int i = 0; estados_anteriores[i] < marca_de_fin; i++) {
+    nuevo->todos[i] = estados_anteriores[i];
+    in++;
+  }
+
+  //una vez que se cargaron los datos de los nodos anteriores, se cargan los datos del nodo actual (posiion de los discos y la cantidad de discos por torre) para lo 
+  // que sirve el valor de la variable in
+
+  for (int i = 0; i < ndiscos; i++) {
+    nuevo->todos[in++] = discos_en_a[i];
+  }
+  nuevo->todos[in++] = cantidad_discos_a;
+
+
+  for (int i = 0; i < ndiscos; i++) {
+    nuevo->todos[in++] = discos_en_b[i];
+  }
+  nuevo->todos[in++] = cantidad_discos_b;
+
+
+  for (int i = 0; i < ndiscos; i++) {
+    nuevo->todos[in++] = discos_en_c[i];
+  }
+  nuevo->todos[in++] = cantidad_discos_c;
+
+  // una vez cragados los datos se coloca el valor de fin en el arreglo
+  nuevo->todos[in++] = marca_de_fin;
+
+  if (*cola == NULL)
+    nuevo->sig = nuevo;
+  else {
+    nuevo->sig = (*cola)->sig;
+    (*cola)->sig = nuevo;
+  }
+  (*cola) = nuevo;
+  max++;
+}
+
+//al eliminar un nodo la variable max se decrementa , porque esta variable 
+// almacena el valor total de los nodos en la lista
+
+void eliminar(struct estado **c1) {
+  int elem = 0;
+  struct estado *aux;
+  if ((*c1) == (*c1)->sig) {
+    free(*c1);
+    *c1 = NULL;
+  } else {
+    aux = (*c1)->sig;
+    (*c1)->sig = aux->sig;
+    free(aux);
+  }
+  max--;
+}
